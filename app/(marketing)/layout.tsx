6fd5fa06eacf09ebface19fd0d945c981bb52597
Mathesis <mateusz.kownacki@gmail.com>
2023-08-30 10:01:54 +0200

    Stworzenie routingu aplikacji

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,12 @@
+export default function MarketingLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div>
+      <header>Marketing header</header>
+      {children}
+    </div>
+  )
+}
