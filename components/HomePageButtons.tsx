e99a1be24936b565621811acbea10607a8a84af9
mathesis <mat@thesis.local>
2023-09-07 09:04:40 +0200

    Stylizacja landing page

--- components/HomePageButtons.tsx
+++ components/HomePageButtons.tsx
@@ -1,7 +1,7 @@
-'use client';
+"use client";
 
-import { UserButton, useUser } from "@clerk/nextjs"
-import Link from "next/link"
+import { UserButton, useUser } from "@clerk/nextjs";
+import Link from "next/link";
 import { Route } from "next";
 
 export default function HomePageButtons() {
@@ -11,16 +11,29 @@ export default function HomePageButtons() {
     <div className="flex items-center gap-4">
       {isSignedIn ? (
         <>
-          <Link href={'/dashboard'}>Dashboard</Link>
+          <Link href={"/dashboard"}>Dashboard</Link>
           <UserButton afterSignOutUrl="/" />
         </>
       ) : (
-        <div>
-          <Link href={'/sign-in' as Route}>SignIn</Link>
-          <Link href={'/sign-up' as Route}>SignUp</Link>
+        <div className="flex gap-2">
+          <Link
+            className="flex w-full text-zinc-800 items-center 
+                    justify-center text-sm whitespace-nowrap font-medium leading-none p-4 mt-4 
+                    rounded-lg border-[1px] border-zinc-500"
+            href={"/sign-in" as Route}
+          >
+            Sign In
+          </Link>
+          <Link
+            className="flex w-full text-zinc-50 bg-zinc-800 whitespace-nowrap items-center text-sm 
+                    justify-center font-medium leading-none p-4 mt-4 
+                    rounded-lg border-[1px] border-zinc-500"
+            href={"/sign-up" as Route}
+          >
+            Sign Up
+          </Link>
         </div>
-      )
-      }
-    </div >
-  )
+      )}
+    </div>
+  );
 }
