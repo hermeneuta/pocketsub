6b17308efd756fb4cfe9d90a0a00d7f1537f5b30
Mathesis <53684759+hermeneuta@users.noreply.github.com>
2023-09-09 22:35:36 +0200

    dodanie dolengo marginesu na stronie głównej

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -22,7 +22,7 @@ export default function HomePage() {
         </Link>
         <Image src={heroImg} alt="Hero image" className="mx-auto mt-10 px-4" />
       </section>
-      <section className="mx-auto max-w-7xl">
+      <section className="mx-auto max-w-7xl mb-20">
         <p className="mt-20 text-center text-xs font-bold uppercase tracking-[2.4px] text-zinc-500">
           What is PocketSub?
         </p>
